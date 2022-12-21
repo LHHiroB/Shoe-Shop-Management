d0807d5c7d4cf138e33b04b935fb1caa8795a87f
LHHiroB <92800179+LHHiroB@users.noreply.github.com>
2022-12-21 21:07:05 +0700

    Delete shoeshop_backend directory

--- shoeshop_backend/middlewares/getFileBuffer.js
+++
@@ -1,7 +0,0 @@
-const DatauriParser = require("datauri/parser");
-const getFileBuffer = (extension, buffer) => {
-  const parser = new DatauriParser();
-  parser.format(extension, buffer);
-  return parser.content;
-};
-module.exports = getFileBuffer;
--- shoeshop_backend/middlewares/multiplefileMulter.js
+++
@@ -1,8 +0,0 @@
-const multer = require("multer");
-
-const path = require("path");
-
-const storage = multer.memoryStorage();
-const multipleMulterUploads = multer({ storage }).array("images", 2);
-
-module.exports = { multipleMulterUploads };
