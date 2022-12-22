1de6b667dade8c4a73df9c3ed50f8e6a6ca42b93
LHHiroB <92800179+LHHiroB@users.noreply.github.com>
2022-12-22 09:40:12 +0700

    Delete shoeshop_backend directory
    Signed-off-by: LHHiroB <92800179+LHHiroB@users.noreply.github.com>

--- shoeshop_backend/middlewares/gererateQR.js
+++
@@ -1,9 +0,0 @@
-var QRCode = require("qrcode");
-const generateQR = async (text) => {
-  try {
-    return await QRCode.toDataURL(text);
-  } catch (err) {
-    console.error(err);
-  }
-};
-module.exports = generateQR;
